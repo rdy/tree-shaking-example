ba8677538d0a03510522c7442821cbce65a6f796
Ryan Dy <ryan.dy@sony.com>
2018-04-11 10:28:16 -0700

    WIP use webpack3 branch for tree shaking for better error messages

--- webpack.haul.js
+++ webpack.haul.js
@@ -1,25 +1,22 @@
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import NormalModuleReplacementPlugin from 'webpack/lib/NormalModuleReplacementPlugin';
 
 module.exports = ({ platform }, defaults) => {
   const bundleTest = /\.(js|(js)?bundle)($|\?)/i;
   const uglifyPlugin =  new UglifyJsPlugin({test: bundleTest, sourceMap: true});
 
+  const replaceReactNativeImplementation = new NormalModuleReplacementPlugin(
+    new RegExp(require.resolve('react-native/Libraries/react-native/react-native-implementation')),
+    require.resolve('./src/vendor/react-native/Libraries/react-native/react-native-implementation.js'),
+  );
+
   return {
+    ...defaults,
     entry: `./index.js`,
-    optimization: {
-      ...defaults.optimization,
-      namedModules: false,
-    },
     plugins: [
       ...defaults.plugins,
       uglifyPlugin,
+      replaceReactNativeImplementation,
     ],
-    resolve: {
-      ...defaults.resolve,
-      alias: {
-        ...defaults.resolve.alias,
-        'react-native$': './src/modules/react-native/Libraries/react-native/react-native-implementation.js'
-      }
-    }
   }
 };
